8f22ac7486f9aa0f4222fdf01265fdbd9beddfda
Sabrina Tessier <s_tessier2@fanshaweonline.ca>
2022-04-25 23:40:11 -0400

    hero video preview done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import logo from './assets/images/logo.png';
+import heroVideo from './assets/video/hero.mp4';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+      <img src={logo} className="App-logo" alt="ThieLot Painting Logo" />
+      <p className="nav-slogan">Getting the Job Done <span className="nav-emphasis">Right</span> the <span className="nav-emphasis">First Time</span></p>
+      <div className="nav-tel">
+        <p>(519) 859-9458</p>
+        <p>Call Now for a <span className="nav-emphasis">Free</span> Estimate</p>
+      </div>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="video-overlay">
+          <div>
+            <h1>ThieLot Painting<span>.</span></h1>
+            <p>Premiere Residential and Commercial Painting Services</p>
+          </div>
+
+        </div>
+        <video src={heroVideo} autoPlay muted></video>
+
+  
+
+
+
+        
+
+  
+    
+         
       </header>
     </div>
   );
