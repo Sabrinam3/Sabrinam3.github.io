7afd61424f7739544743db6cc6fbbc5c9ad83889
Sabrina Tessier <s_tessier2@fanshaweonline.ca>
2022-04-27 22:51:31 -0400

    fix nav

--- src/App.js
+++ src/App.js
@@ -6,60 +6,26 @@ import phone from './assets/images/phone.svg';
 
 import './App.css';
 
-document.addEventListener("scroll", stickyNav);
-function stickyNav() {
-  const isMobile = !window.matchMedia('only screen and (min-width: 600px)').matches;
-  const secondaryNav = document.getElementById("secondary-nav");
-  const secondaryNavMobile = document.getElementById("secondary-nav-mobile");
-  if(isMobile)
-  {
-    if(window.pageYOffset > 90)
-    {
-      secondaryNavMobile.style.position = "fixed";
-      secondaryNavMobile.style.top = "0";
-      secondaryNavMobile.style.left = "0";
-    }
-    else
-    {
-      secondaryNavMobile.style.position = "inherit";
-    }
-  }
-  else{
-    if(window.pageYOffset > 125)
-    {
-      secondaryNav.style.display = "block";
-      secondaryNav.style.position = "fixed";
-      secondaryNav.style.top = "0";
-      secondaryNav.style.left = "0";
-    }
-    else
-    {
-      secondaryNav.style.display = "none";
-    }
-  } 
-}
 function App() {
   return (
     
     <div className="App">
       <nav>
-      <img src={logo} className="App-logo" alt="ThieLot Painting Logo" />
-      <p className="nav-slogan">Doing the Project Properly the <span className="nav-emphasis">First Time</span></p>
-      <div className="nav-tel">
-        <div className="contact-mobile-span">
-          <button className="button1">Contact Us</button>
+        <img src={logo} className="App-logo" alt="ThieLot Painting Logo" />
+        <p className="nav-slogan">Doing the Project Properly the <span className="nav-emphasis">First Time</span></p>
+        <div className="nav-tel">
+          <div className="contact-mobile-span">
+            <button className="button1">Contact Us</button>
+          </div>
+          <p>(519) 859-9458</p>
+          <p>Call Now for a <span className="nav-emphasis">Free</span> Estimate</p>
         </div>
-        <p>(519) 859-9458</p>
-        <p>Call Now for a <span className="nav-emphasis">Free</span> Estimate</p>
+        <div className="contact-mobile-bar">
+          <img src={phone} alt="Call Now Icon"/>
+          <p>Call Now for a <span className="nav-emphasis">Free</span> Estimate</p>
       </div>
       </nav>
-      <div className="contact-mobile-bar" id="secondary-nav-mobile">
-        <img src={phone} alt="Call Now Icon"/>
-        <p>Call Now for a <span className="nav-emphasis">Free</span> Estimate</p>
-      </div>
-      <div id="secondary-nav">
-      <img src={logo} className="App-logo" alt="ThieLot Painting Logo" />
-    </div>
+
       <header className="App-header">
         <div className="video-overlay">
           <div>
