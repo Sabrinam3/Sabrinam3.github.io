7c1a971852cf0fa903ec61bbb69b4d4306936a69
Sabrina Tessier <s_tessier2@fanshaweonline.ca>
2022-04-28 22:38:58 -0400

    mission section started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './assets/images/logo.png';
 import logosmall from './assets/images/logo-small.png';
 import heroVideo from './assets/video/hero.mp4';
 import phone from './assets/images/phone.svg';
+import ladder from './assets/images/ladder.jpeg';
+
+
 
 
 import './App.css';
@@ -12,7 +15,7 @@ function App() {
     <div className="App">
       <nav>
         <img src={logo} className="App-logo" alt="ThieLot Painting Logo" />
-        <p className="nav-slogan">Doing the Project Properly the <span className="nav-emphasis">First Time</span></p>
+        {/* <p className="nav-slogan">Doing the Project Properly the <span className="nav-emphasis">First Time</span></p> */}
         <div className="nav-tel">
           <div className="contact-mobile-span">
             <button className="button1">Contact Us</button>
@@ -37,20 +40,28 @@ function App() {
           </div>
         </div>
           <video src={heroVideo} type="video/mp4" autoPlay muted loop playsInline ></video>
-        <main>
-          <h2></h2>
-        </main>
-
-  
-
-
-
-        
-
-  
-    
-         
       </header>
+      <main>
+        <div className="inner-content">
+         
+            <div className="subheading-bar">
+              <h2>Our Mission</h2>
+            </div>
+          <div className='mission-section'>
+            <div className="mission-statement">
+              <div>
+                 <h3 className="slogan">Doing the Project <span className="nav-emphasis">Properly</span> the <span className="nav-emphasis">First Time</span></h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem lacus, finibus eget ligula id, accumsan volutpat tortor. Suspendisse id odio congue nisi pretium fringilla sed et lorem. Vestibulum quis dolor libero. Vivamus et auctor felis, quis scelerisque quam. Duis nec sem lacinia, pretium dolor in, tristique metus. Proin suscipit, elit non egestas ullamcorper, mauris ipsum pellentesque tortor, eget luctus enim erat in nisl. Sed ex ante, feugiat non orci eu, posuere iaculis dui. </p>
+              </div>
+            </div>
+           
+            <img src={ladder} alt="a bedroom is being painted white"/>
+            
+          </div>
+
+        </div>
+
+      </main>
     </div>
   );
 }
