d320628613c238e3b62cdf8aaae5f4def0e00456
Sabrina Tessier <s_tessier2@fanshaweonline.ca>
2022-05-14 15:21:45 -0400

    services section mostly done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import handshake from './assets/images/handshake.svg';
 import paintbrush from './assets/images/paintbrush.svg';
 import invoice from './assets/images/invoice.svg';
 import arrowright from './assets/images/arrow-right.svg';
+import interior from "./assets/images/interior.jpeg";
+import interior1 from "./assets/images/interior1.jpeg";
+import interior2 from "./assets/images/interior2.jpeg";
+
+import exterior from "./assets/images/exterior.jpeg";
+import exterior1 from "./assets/images/exterior1.jpeg";
+import exterior2 from "./assets/images/exterior2.jpeg";
+
+import commercial1 from "./assets/images/commercial1.jpeg";
+import commercial2 from "./assets/images/commercial2.jpeg";
+import commercial3 from "./assets/images/commercial3.jpeg";
+
 
 
 
@@ -47,15 +59,16 @@ function App() {
       <main>
           <section>
               <h2 className="subheading">Our Mission</h2>
-            <div className="mission-statement">
+              <div className="mission-statement">
+                  <div>
                   <h3 className="slogan">Doing the Project Properly the <span className="nav-emphasis">First Time</span></h3>
                   <p>At ThieLot Painting, we take pride in every project - that is why we bring top quality workmanship and meticulous attention to detail to every job - large or small.</p>
                   <p>We remain dedicated to delivering the <span className="nav-emphasis">best quality</span> at the most <span className="nav-emphasis">competitive price</span> by combining current technology with old fashioned values and service.</p>
                   <p>We are a family run business with over x years of industry experience and knowledge. Let us help make your home or business look its best! </p>
                   <button className="button1">Contact Us Today</button>
-           
-                {/* <img src={ladder} alt="a bedroom is being painted white"/> */}
- 
+                  </div>
+
+                  <img src={ladder} alt="a bedroom is being painted white"/>
             </div>
           
           </section>
@@ -96,19 +109,210 @@ function App() {
           </section>
         
           <section className="services-section">
-          
-          <div className="services-container">
-          <h2 className="subheading"><span className="nav-emphasis">Residential</span> Painting </h2>
-          </div>
+            <div className="services-top-bar">
+              <div>
+                <img src={logosmall} alt=""/>
+                <h2 className="subheading"><span className="nav-emphasis-dark">Residential</span> Painting Services</h2>
+              </div>
+       
+            </div>
+            <h4>The <span className="nav-emphasis-dark">ThieLot Painting </span>Difference</h4>
+              <div className="services-list-wide">
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam.</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam.</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam.</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam.</p>
+                </div>   
+              </div>
+            <div className="services-overlay">
+              <div className="services-img">
+                  <div className="services-img-overlay">
+                      <h4>Residential Painting Services</h4>
+                      <button className="button1">Free Quote</button>
+                  </div>
+                  <img src={interior} alt=""/>
+              </div>
+              <div className="services-img">
+                  <div className="services-img-overlay">
+                    <h4>Residential Painting Services</h4>
+                    <button className="button1">Free Quote</button>
+                  </div>
+                  <img src={exterior1} alt=""/>
+              </div>
+              <div className="services-img">
+                <div className="services-img-overlay">
+                    <h4>Residential Painting Services</h4>
+                    <button className="button1">Free Quote</button>
+                </div>
+                <img src={exterior} alt=""/>
+              </div>
+            </div>
+             
+            {/* Interior Painting */}
+             
+            <div className="services-sub-text">
+              <h3><span className="nav-emphasis-dark">Interior</span> Painting</h3>   
+              <h4>Our Services Include:</h4>
+              <div className="services-list-container">
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam. </p>
+              <button className="button1">Free Interior Painting Quote</button>
+            </div>
+
+            {/* Exterior Painting */}
+            <div className="services-sub-text">
+              <h3><span className="nav-emphasis-dark">Exterior</span> Painting</h3>   
+              <h4>Our Services Include:</h4>
+              <div className="services-list-container">
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam. </p>
+              <button className="button1">Free Exterior Painting Quote</button>
+            </div>
+                  
           </section>
-          <section className="estimate-section">
+          {/* <section className="estimate-section">
             <div className="estimate-overlay">
               <h2>Book Your Free Estimate Today!</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque eget lorem vestibulum, faucibus odio et, maximus orci. Curabitur nec tellus feugiat, semper ex vel, dictum leo.</p>
               <button className="icon-button">Get a Free Estimate<img src={arrowright} alt=""/></button>
             </div>
-          </section>
-
+          </section> */}
+           <section className="services-section">
+           <div className="services-top-bar">
+              <div>
+                <img src={logosmall} alt=""/>
+                <h2 className="subheading"><span className="nav-emphasis-dark">Commercial</span> Painting Services</h2>
+              </div>
+       
+            </div>
+            <div className="services-overlay">
+              <div className="services-img">
+                  <div className="services-img-overlay">
+                      <h4>Commercial Painting Services</h4>
+                      <button className="button1">Free Quote</button>
+                  </div>
+                  <img src={commercial1} alt=""/>
+              </div>
+              <div className="services-img">
+                  <div className="services-img-overlay">
+                    <h4>Commercial Painting Services</h4>
+                    <button className="button1">Free Quote</button>
+                  </div>
+                  <img src={commercial2} alt=""/>
+              </div>
+              <div className="services-img">
+                <div className="services-img-overlay">
+                    <h4>Commercial Painting Services</h4>
+                    <button className="button1">Free Quote</button>
+                </div>
+                <img src={commercial3} alt=""/>
+              </div>
+            </div>
+            {/* Commercial Painting Services */}
+            <div className="services-sub-text">
+              <h3><span className="nav-emphasis-dark">Commercial</span> Painting</h3>   
+              <h4>Our Services Include:</h4>
+              <div className="services-list-container">
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+                <div className="services-list-item">
+                  <img src={paintbrush} alt=""/>
+                  <p>Interior Wall Painting</p>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante lobortis, vulputate lectus quis, commodo diam. </p>
+              <button className="button1">Free Commercial Painting Quote</button>
+            </div>
+           </section>
       </main>
     </div>
   );
